26d8c5d467a8c94f63ab891ff59e61092bb9aff4
Christoph Huber <huber.pierbach@gmail.com>
2021-03-12 13:19:19 +0100

    added toucheffect isse project

---
+++ TouchEffectNullReference/TouchEffectNullReference/TouchEffectNullReference/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.CommunityToolkit.Effects;
+using Xamarin.Forms;
+
+namespace TouchEffectNullReference
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var absoluteLabel = new Label {Text = "Absolute Navigation", HeightRequest = 50};
+
+            TouchEffect.SetCommandParameter(absoluteLabel, "Absolute");
+            TouchEffect.SetCommand(absoluteLabel, new Command((obj) =>
+            {
+                Console.WriteLine(obj);
+                Application.Current.MainPage = new MainPage();
+            }));
+
+            var relativeLabel = new Label {Text = "Relative Navigation", HeightRequest = 50};
+
+            TouchEffect.SetCommandParameter(relativeLabel, "Relative");
+            TouchEffect.SetCommand(relativeLabel, new Command(async (obj) =>
+            {
+                Console.WriteLine(obj);
+                await Navigation.PushAsync(new MainPage());
+            }));
+
+            var goBackLabel = new Label {Text = "Back Navigation", HeightRequest = 50};
+
+            TouchEffect.SetCommandParameter(goBackLabel, "Back");
+            TouchEffect.SetCommand(goBackLabel, new Command(async (obj) =>
+            {
+                Console.WriteLine(obj);
+                await Navigation.PopAsync();
+            }));
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    absoluteLabel, relativeLabel, goBackLabel
+                }
+            };
+        }
+    }
+}
